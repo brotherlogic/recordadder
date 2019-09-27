217284a597ed8bc7f5cdb5738455b002536d29a3
Simon Tucker <simon@brotherlogic@gmail>
2019-09-26 20:54:02 -0700

    Bounces

--- recordadder.go
+++ recordadder.go
@@ -52,7 +52,7 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
 	return []*pbg.State{
-		&pbg.State{Key: "hello", Value: int64(123)},
+		&pbg.State{Key: "hello", Value: int64(1234)},
 	}
 }
 
