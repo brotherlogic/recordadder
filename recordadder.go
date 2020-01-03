a1b92dec9659ea5eb8aec9d6a8b4556d3f09d2d6
Simon Tucker <simon@brotherlogic@gmail>
2020-01-03 15:28:12 -0800

    Resets the score on move

--- recordadder.go
+++ recordadder.go
@@ -48,7 +48,7 @@ func (p *prodCollection) addRecord(ctx context.Context, r *pb.AddRecordRequest) 
 	// Folder reset
 	_, err = client.UpdateRecord(ctx, &pbrc.UpdateRecordRequest{Update: &pbrc.Record{
 		Release:  &pbgd.Release{InstanceId: r.GetId()},
-		Metadata: &pbrc.ReleaseMetadata{GoalFolder: r.GetResetFolder()},
+		Metadata: &pbrc.ReleaseMetadata{GoalFolder: r.GetResetFolder(), SetRating: -1},
 	}})
 	return err
 }
