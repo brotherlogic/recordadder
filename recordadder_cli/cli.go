610286db9ecc8c529d67a8719f83a08b7e264c88
Simon Tucker <brotherlogic@gmail.com>
2020-07-20 17:22:26 -0700

    Fix

--- recordadder_cli/cli.go
+++ recordadder_cli/cli.go
@@ -42,7 +42,7 @@ func main() {
 		var year = addFlags.Int("year", time.Now().Year(), "Year for accounting purposes")
 
 		if err := addFlags.Parse(os.Args[2:]); err == nil {
-			if id > 0 && cost > 0 && folder > 0 {
+			if *id > 0 && *cost > 0 && *folder > 0 {
 				res, err := client.AddRecord(ctx, &pb.AddRecordRequest{Cost: int32(*cost), Id: int32(*id), Folder: int32(*folder), ResetFolder: int32(*resetFoldr), AccountingYear: int32(*year)})
 				if err != nil {
 					log.Fatalf("Error on Add Record: %v", err)
