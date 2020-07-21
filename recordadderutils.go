d8f00ef2d21f3fd4c9a62d9494742e7065eefde4
Simon Tucker <brotherlogic@gmail.com>
2020-07-20 17:21:22 -0700

    Makes a queue adjustment on bad ids

--- recordadderutils.go
+++ recordadderutils.go
@@ -26,6 +26,11 @@ func (s *Server) processQueue(ctx context.Context) error {
 
 	if len(queue.Requests) > 0 && time.Now().Sub(time.Unix(queue.LastAdditionDate, 0)) >= time.Hour*24 {
 		for i, req := range queue.GetRequests() {
+			if req.GetId() <= 0 {
+				queue.Requests = append(queue.Requests[:i], queue.Requests[i+1:]...)
+				s.KSclient.Save(ctx, QUEUE, queue)
+				return fmt.Errorf("Bad entry in the queue")
+			}
 			if req.GetCost() < available {
 				err = s.rc.addRecord(ctx, queue.Requests[i])
 				s.Log(fmt.Sprintf("Adding %v -> %v", queue.Requests[i], err))
