752efbd5b145528de38d4ffcba9db4a502d9d2ff
Simon Tucker <brotherlogic@gmail.com>
2020-10-23 15:34:13 -0700

    Logging out more detail

--- recordadderutils.go
+++ recordadderutils.go
@@ -23,7 +23,7 @@ func (s *Server) processQueue(ctx context.Context) error {
 	}
 
 	available := budget.GetBudget() - budget.GetSpends()
-	s.Log(fmt.Sprintf("Found %v entries in the queue with %v in the budget (%v)", len(queue.Requests), available, time.Now().Sub(time.Unix(queue.LastAdditionDate, 0)) >= time.Hour*24))
+	s.Log(fmt.Sprintf("Found %v entries in the queue with %v in the budget (%v) -> %v", len(queue.Requests), available, time.Now().Sub(time.Unix(queue.LastAdditionDate, 0)) >= time.Hour*24, ctx))
 	if len(queue.Requests) > 0 && time.Now().Sub(time.Unix(queue.LastAdditionDate, 0)) >= time.Hour*24 {
 		for i, req := range queue.GetRequests() {
 			if req.GetId() <= 0 || req.GetResetFolder() > 0 {
