da6b858a124edf8a1b2af5ba0f51ca34bc75eb83
Simon Tucker <brotherlogic@gmail.com>
2024-02-27 08:50:59 -0800

    Update CDs every minute

--- recordadderapi.go
+++ recordadderapi.go
@@ -139,7 +139,7 @@ func (s *Server) ProcAdded(ctx context.Context, req *pb.ProcAddedRequest) (*pb.P
 	val, ok := conf.GetAddedMap()[req.GetType()]
 	//s.CtxLog(ctx,fmt.Sprintf("ADDED the MAP: %v (%v)", time.Since(time.Unix(val, 0)), time.Unix(val, 0)))
 	if !ok || time.Since(time.Unix(val, 0)) > time.Hour*24 ||
-		(time.Since(time.Unix(val, 0)) > time.Hour*6 && req.GetType() == "FILE_CD") ||
+		(time.Since(time.Unix(val, 0)) > time.Minute && req.GetType() == "FILE_CD") ||
 		(time.Since(time.Unix(val, 0)) > time.Hour*6 && req.GetType() == "FILE_12_INCH") {
 		//s.CtxLog(ctx,"Adding!")
 
