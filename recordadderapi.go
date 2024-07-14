d05b7caf59cbd28a676ea37c786f56ca68e4e666
Simon Tucker <brotherlogic@gmail.com>
2024-07-13 17:04:27 -0700

    Fix today

--- recordadderapi.go
+++ recordadderapi.go
@@ -149,6 +149,7 @@ func (s *Server) ProcAdded(ctx context.Context, req *pb.ProcAddedRequest) (*pb.P
 
 	if time.Now().YearDay() != int(conf.GetCurrentDay()) {
 		conf.TodayFolders = make(map[int32]int32)
+		conf.CurrentDay = int32(time.Now().YearDay())
 	}
 
 	s.CtxLog(ctx, fmt.Sprintf("Found %v", conf.GetTodayFolders()))
